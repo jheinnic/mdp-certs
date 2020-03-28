ca409d1864e1602055aa01241f54214bf8128417
John Heinnickel` <jheinnic@gmail.com>
2020-03-28 09:54:11 -0700

    Fix errors in some scripts

--- CA/makeCACert.sh
+++ CA/makeCACert.sh
@@ -8,6 +8,6 @@ openssl x509 -signkey cakey.pem -req -days 3650 -in careq.pem -out caroot.cer -e
 keytool -printcert -v -file caroot.cer
 # keytool -list -protected -keystore cacerts
 
-openssl x509 -CA caroot.cer -CAkey cakey.pem -CAserial serial.txt -req -in ../`hostname`.csr -out ../`hostaname`-CASigned.cer -days 3650
+openssl x509 -CA caroot.cer -CAkey cakey.pem -CAserial serial.txt -req -in ../`hostname`.csr -out ../`hostname`-CASigned.cer -days 3650 -passin pass:abc123
 
 # -config C:\OpenSSL-Win64\bin\openssl.cfg
--- jksToP12.sh
+++ jksToP12.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 keytool -importkeystore -srckeystore blank.jks -srcstorepass abc123 -destkeystore bluemix-CA.p12 -deststoretype PKCS12 -srcalias TestCA -deststorepass abc123 
-keytool -importkeystore -srckeystore blank.jks -srcstorepass abc123 -destkeystore bluemix-app.p12 -deststoretype PKCS12 -srcalias `hostname` -deststorepass abc123 -destalias `hostname`-signed
+keytool -importkeystore -srckeystore `hostname`.jks -srcstorepass abc123 -destkeystore bluemix-app.p12 -deststoretype PKCS12 -srcalias `hostname` -deststorepass abc123 -destalias `hostname`-signed
 keytool -importkeystore -srckeystore `hostname`.jks -srcstorepass abc123 -srckeypass abc123 -srcalias `hostname` -destkeystore bluemix-app.p12 -deststoretype PKCS12 -deststorepass abc123 -destkeypass abc123
 
 # Now use openssl to split the PKCS12 file created above into first, the certificate file, and then the private key file. While the CA-issued certificate can be used as is, the command has been provided here for completeness.
